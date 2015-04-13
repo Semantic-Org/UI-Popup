463d30fd2b7e9e3bf150c847c2d3b7672584a7a3
Pusher Robot <jack@semantic-ui.com>
2015-04-13 15:23:43 -0400

    Updated component to version 1.11.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-popup',
   summary : 'Semantic UI - Popup: Single component release',
-  version : '1.11.6',
+  version : '1.11.7',
   git     : 'git://github.com/Semantic-Org/UI-Popup.git',
 });
 
