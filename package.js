01aab72544126c05950fb5b6befcf52233cb295f
Jack Lukic <jack@semantic-ui.com>
2017-02-21 01:13:35 -0500

    Updated component to version 2.2.8

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-popup',
   summary : 'Semantic UI - Popup: Single component release',
-  version : '2.2.7',
+  version : '2.2.8',
   git     : 'git://github.com/Semantic-Org/UI-Popup.git',
 });
 
